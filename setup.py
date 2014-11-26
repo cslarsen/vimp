9422873c31ebb133fae768212defd4629633cad9
Christian Stigen Larsen <csl@sublevel3.org>
2014-11-26 22:38:33 +0100

    Removes README.txt and dependency

--- setup.py
+++ setup.py
@@ -10,8 +10,5 @@ setup(
     url='https://github.com/cslarsen/vimp',
     license='LICENSE.txt',
     description='Command-line package manager or vim.',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "Dulwhich >= 0.9.6",
-    ],
+    long_description=open('README.md').read(),
 )
