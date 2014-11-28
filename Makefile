8a1d414413606557237196d0a8adbeb68e9ad296
Christian Stigen Larsen <csl@sublevel3.org>
2014-11-28 20:15:19 +0100

    Changes setup.py to use setuptools

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ check:
 clean:
 	find . -name '*.pyc' | xargs rm -f
 	find . -name '*.pyo' | xargs rm -f
-	rm -rf build dist test MANIFEST
+	rm -rf build dist test MANIFEST vimp.egg-info/
--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='vimp',
-    version='0.0.1',
-    author='Christian Stigen Larsen',
-    author_email='csl@csl.name',
-    packages=['vimp', 'vimp.test'],
-    scripts=['bin/vimp'],
-    url='https://github.com/cslarsen/vimp',
-    license='LICENSE.txt',
-    description='Command-line package manager or vim.',
-    long_description=open('README.md').read(),
+    name="vimp",
+    version="0.0.1",
+    author="Christian Stigen Larsen",
+    author_email="csl@csl.name",
+    keywords="vim",
+    packages=["vimp", "vimp.test"],
+    scripts=["bin/vimp"],
+    url="https://github.com/cslarsen/vimp",
+    license="LICENSE.txt",
+    description="Command-line package manager or vim.",
+    long_description=open("README.md").read(),
+    #install_requires=["dulwich"],
 )
