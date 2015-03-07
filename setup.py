cc7225ff79ce3a4400e7392fcf9c82ba0b9c36c2
Christian Stigen Larsen <csl@sublevel3.org>
2015-03-07 13:49:41 +0100

    Fixes description

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ except ImportError:
 
 setup(
     name="vimp",
+    description="Command line package manager for the vim editor.",
     packages=["vimp"],
     version="0.1.2",
     author="Christian Stigen Larsen",
@@ -14,7 +15,6 @@ setup(
     url="https://github.com/cslarsen/vimp",
     download_url="https://github.com/cslarsen/vimp/tarball/0.1.2",
     license="https://www.gnu.org/licenses/lgpl-2.1.html",
-    description="Command-line package manager or vim.",
     long_description=open("README.rst").read(),
     zip_safe=True,
     test_suite="vimp.test",
